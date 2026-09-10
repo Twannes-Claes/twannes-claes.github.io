788604a90eeb3668d23510274c5a60bba0483040
Twannes Claes <104838597+Twannes-Claes@users.noreply.github.com>
2026-09-10 20:52:36 +0200

    fix(theme): use light theme of user's machine

--- assets/js/theme-init.js
+++ assets/js/theme-init.js
@@ -1,1 +1,7 @@
-document.documentElement.setAttribute('data-theme', localStorage.getItem('theme') || 'dark');
+(function ()
+{
+    var stored = localStorage.getItem('theme');
+    var systemPrefersLight = window.matchMedia('(prefers-color-scheme: light)').matches;
+    var theme = stored || (systemPrefersLight ? 'light' : 'dark');
+    document.documentElement.setAttribute('data-theme', theme);
+})();
